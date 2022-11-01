9bd59f8bf59ca95693b4e67c6d9593a14c19a681
yuta-hidaka <yuta322@gmail.com>
2022-11-01 15:47:26 +0900

    Add sample code

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,54 @@
 import { render } from "solid-js/web";
+import { Event } from "./main";
 
-const App = () => <div>Hello world with Solid js with vite</div>;
+const App = () => {
+  const dispatch = ({ type, payload }: Event) => {
+    // Send message plugin to figma
+    parent.postMessage({ pluginMessage: { type, payload } }, "*");
+  };
 
-render(() => <App />, document.getElementById("root"));
+  return (
+    <div>
+      <div style="margin:10px">
+        <button
+          onClick={() =>
+            dispatch({
+              type: "CREATE/FRAME",
+              payload: { width: 500, height: 500 },
+            })
+          }
+        >
+          Create frame
+        </button>
+      </div>
+
+      <div style="margin:10px">
+        <button
+          onClick={() =>
+            dispatch({
+              type: "CREATE/RECTANGLE",
+              payload: { width: 100, height: 100 },
+            })
+          }
+        >
+          Create rectangle
+        </button>
+      </div>
+
+      <div style="margin:10px">
+        <button
+          onClick={() =>
+            dispatch({
+              type: "PLUGIN/CLOSE",
+              payload: undefined,
+            })
+          }
+        >
+          Close plugin
+        </button>
+      </div>
+    </div>
+  );
+};
+
+render(() => <App />, document.getElementById("root") as HTMLElement);
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
+export type Event =
+  | {
+      type: "CREATE/RECTANGLE";
+      payload: { width: number; height: number };
+    }
+  | {
+      type: "CREATE/FRAME";
+      payload: { width: number; height: number };
+    }
+  | {
+      type: "PLUGIN/CLOSE";
+      payload: undefined;
+    };
+
 async function onRun() {
   figma.showUI(__html__, {
     width: 500,
@@ -5,13 +19,81 @@ async function onRun() {
   });
 }
 
-function onMessage(event: any) {
+function onMessage(event: Event) {
   console.log("On message fired");
   console.log("event: ", event);
+
+  const { type, payload } = event;
+
+  switch (type) {
+    case "CREATE/RECTANGLE": {
+      // Create rectangle
+      const rect = figma.createRectangle();
+      // Set size
+      rect.resize(payload.width, payload.height);
+      // Set solid red fill
+      rect.fills = [
+        {
+          type: "GRADIENT_DIAMOND",
+          gradientStops: [
+            { position: 0, color: { r: 1, g: 0.4, b: 0.4, a: 1 } },
+            { position: 1, color: { r: 1, g: 0.7, b: 0.4, a: 1 } },
+          ],
+          gradientTransform: [
+            [1, 0, 0],
+            [0, 1, 0],
+          ],
+        },
+      ];
+      // Focus to created node
+      figma.currentPage.selection = [rect];
+      break;
+    }
+    case "CREATE/FRAME": {
+      // Create frame
+      const rect = figma.createFrame();
+      // Set size
+      rect.resize(payload.width, payload.height);
+      // Set solid red fill
+      rect.fills = [
+        {
+          type: "GRADIENT_DIAMOND",
+          gradientStops: [
+            { position: 0, color: { r: 1, g: 0.7, b: 0.2, a: 1 } },
+            { position: 1, color: { r: 1, g: 0.9, b: 0.4, a: 1 } },
+          ],
+          gradientTransform: [
+            [1, 0, 0],
+            [0, 1, 0],
+          ],
+        },
+      ];
+      // Focus to created node
+      figma.currentPage.selection = [rect];
+      break;
+    }
+    case "PLUGIN/CLOSE": {
+      figma.closePlugin();
+    }
+  }
 }
 
 function main() {
+  // For debug
+  console.clear();
+
+  // Figma event setup
+
+  // when plugin started
   figma.on("run", onRun);
+
+  // when figma"s selection has been changed
+  figma.on("selectionchange", () => {
+    const currentNode = figma.currentPage.selection;
+
+    console.log();
+  });
+
   figma.ui.onmessage = onMessage;
 }
 main();
